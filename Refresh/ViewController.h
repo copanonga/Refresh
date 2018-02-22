4840ba03758bb9b1bffcc7ddccbf42de95256f21
José Manuel Suárez Gutiérrez <jose.suarez@tippingpoint.es>
2018-02-22 15:37:15 +0100

    First commit

---
+++ Refresh/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Refresh
+//
+//  Created by PROIMA on 22/2/18.
+//  Copyright © 2018 Copanonga. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Refresh/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Refresh
+//
+//  Created by PROIMA on 22/2/18.
+//  Copyright © 2018 Copanonga. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+	@autoreleasepool {
+	    return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+	}
+}
