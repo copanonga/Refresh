43a0d2ad8cca78fe8a1a33c2ad72635262779d04
José Manuel Suárez Gutiérrez <jose.suarez@tippingpoint.es>
2018-02-22 16:00:08 +0100

    Creada tabla

--- Refresh/ViewController.m
+++ Refresh/ViewController.m
@@ -8,20 +8,75 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+@interface ViewController () {
+	NSMutableArray *datosTabla;
+}
+@property (strong, nonatomic) IBOutlet UITableView *tablaResultados;
 
 @end
 
 @implementation ViewController
+@synthesize tablaResultados;
 
 - (void)viewDidLoad {
 	[super viewDidLoad];
+	[self inicializarDatos];
+	[tablaResultados reloadData];
 }
-
 
 - (void)didReceiveMemoryWarning {
 	[super didReceiveMemoryWarning];
 }
 
+#pragma mark - Datos
+
+-(void)inicializarDatos {
+	
+	datosTabla = [[NSMutableArray alloc] init];
+	
+	for (int i = 0 ; i < 10 ; i++){
+		NSString *dato = [NSString stringWithFormat:@"Dato: %i",i];
+		[datosTabla addObject:dato];
+	}
+	
+}
+
+#pragma mark - TableView
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
+{
+	return 1;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+	
+	return [datosTabla count];
+	
+}
+
+- (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+	return 90;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+	
+	UITableViewCell *cell =  [tableView dequeueReusableCellWithIdentifier:@"celda"];
+	if (!cell) {
+		cell = [[UITableViewCell alloc]initWithStyle:UITableViewCellStyleDefault reuseIdentifier:@"celda"];
+	}
+	
+	cell.textLabel.text = [datosTabla objectAtIndex:indexPath.row];
+	
+	return cell;
+	
+}
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
+{
+	
+}
 
 @end
