e98d0598ee0a6c7b6113581feb7519271709d04a
José Manuel Suárez Gutiérrez <jose.suarez@tippingpoint.es>
2018-02-22 16:09:34 +0100

    Creados métodos para cargar datos de prueba y para recargar de los datos

--- Refresh/ViewController.m
+++ Refresh/ViewController.m
@@ -23,29 +23,14 @@
 
 - (void)viewDidLoad {
 	
+	NSLog(@"\nViewController");
 	[super viewDidLoad];
+	
 	[self inicializarDatos];
+	
 	[tablaResultados reloadData];
 	
-	tableView = tablaResultados;
-	
-	tableView.mj_header= [MJRefreshNormalHeader headerWithRefreshingBlock:^{
-		
-		dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(2.0 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
-			NSLog(@"Pull to refresh");
-			[tableView.mj_header endRefreshing];
-		});
-	}];
-	
-	tableView.mj_header.automaticallyChangeAlpha = YES;
-
-	tableView.mj_footer = [MJRefreshBackNormalFooter footerWithRefreshingBlock:^{
-		
-		dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(2.0 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
-			NSLog(@"Load more");
-			[tableView.mj_footer endRefreshing];
-		});
-	}];
+	[self refrescoPaginacion];
 	
 }
 
@@ -60,9 +45,73 @@
 	datosTabla = [[NSMutableArray alloc] init];
 	
 	for (int i = 0 ; i < 10 ; i++){
-		NSString *dato = [NSString stringWithFormat:@"Dato: %i",i];
+		NSString *dato = [NSString stringWithFormat:@"Data: %i",i];
 		[datosTabla addObject:dato];
 	}
+	
+}
+
+-(void)datosPullToRefresh {
+	
+	datosTabla = [[NSMutableArray alloc] init];
+	
+	for (int i = 0 ; i < 10 ; i++){
+		NSString *dato = [NSString stringWithFormat:@"Pull to refresh: %i",i];
+		[datosTabla addObject:dato];
+	}
+	
+}
+
+-(void)datosLoadMore {
+	
+	for (int i = 0 ; i < 10 ; i++){
+		NSString *dato = [NSString stringWithFormat:@"Load more: %i",i];
+		[datosTabla addObject:dato];
+	}
+	
+}
+
+#pragma mark - Refresh
+
+-(void)refrescoPaginacion {
+	
+	tableView = tablaResultados;
+	
+	tableView.mj_header= [MJRefreshNormalHeader headerWithRefreshingBlock:^{
+		
+		dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(2.0 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+			[self pullToRefresh];
+		});
+	}];
+
+	tableView.mj_header.automaticallyChangeAlpha = YES;
+	
+	tableView.mj_footer = [MJRefreshBackNormalFooter footerWithRefreshingBlock:^{
+		
+		dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(2.0 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+			[self loadMore];
+		});
+	}];
+	
+}
+
+-(void)pullToRefresh {
+	
+	NSLog(@"Pull to refresh");
+	[self datosPullToRefresh];
+	
+	[tablaResultados reloadData];
+	[tableView.mj_header endRefreshing];
+	
+}
+
+-(void)loadMore {
+	
+	NSLog(@"Load more");
+	[self datosLoadMore];
+	
+	[tablaResultados reloadData];
+	[tableView.mj_footer endRefreshing];
 	
 }
 
